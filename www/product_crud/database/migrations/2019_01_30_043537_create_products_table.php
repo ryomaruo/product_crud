bad1ea6ac25206cb99fa2c3d30e91bbf75533513
亮丸尾 <r.maruo182@gmail.com>
2019-01-30 16:05:19 +0900

    First Commit

---
+++ www/product_crud/database/migrations/2019_01_30_043537_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 32);
+            $table->string('model_number', 50);
+            $table->integer('price')->default(0);
+            $table->integer('stock')->default(0);
+            $table->boolean('discontinued');
+            $table->text('discription');
+            $table->string('image_url');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
